10985fa3dd76d3cab57339dfbae55ad584684ace
Shakib Hasan <195508406+imshakibhasan1@users.noreply.github.com>
2026-05-15 07:09:07 -0500

    Change API URL to production endpoint

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://stylegen.onrender.com';
 
 // Create axios instance
 const axiosInstance = axios.create({
